9d45839a5cc96faa7d1edcda08d81d18264b128c
Milosz Bator <miloszbator00@gmail.com>
2025-08-25 19:17:05 +0200

    added tests for DPV exercise 6.1

--- tests/dpv_chapter_6.py
+++ tests/dpv_chapter_6.py
@@ -5,12 +5,23 @@ Follow this format for test cases:
 test_cases = {
     "6.1": [
         ('example_test', [5, 15, -30, 10, -5, 40, 10], [10, -5, 40, 10]),  
-        ('empty_test', [], []),
-        ('empty_test2', [], []),
-        ('empty_test3', [], []),
-        ('empty_test4', [], []),
-        ('empty_test5', [], []),
-        ('empty_test6', [], []),
-        ('empty_test7', [], [])
-    ]
+        ('all_negative', [-1, -2, -3, -4], []),
+        ('all_positive', [1, 2, 3, 4], [1, 2, 3, 4]),
+        ('mixed_negative_start', [-1, 2, 3, -4, 5], [2, 3, -4, 5]),
+        ('mixed_negative_end', [1, -2, 3, 4, -1], [3, 4]),
+        ('single_positive', [5], [5]),
+        ('single_negative', [-3], []),
+        ('zeros', [0, 0, 0], []),
+        ('positive_after_negative', [-1, -2, 3, 4], [3, 4]),
+        ('negative_after_positive', [3, 4, -1, -2], [3, 4]),
+        ('alternating', [1, -2, 3, -4, 5], [5]),
+        ('large_numbers', [1000000, -1, 1000000], [1000000, -1, 1000000]),
+        ('negative_middle', [10, -5, 40, 10], [10, -5, 40, 10]),
+        ('decreasing_sequence', [10, 5, 1, -5, -10], [10, 5, 1]),
+        ('increasing_sequence', [-10, -5, 1, 5, 10], [1, 5, 10])
+    ],
+    "6.2": [],
+    "6.3": [], 
+    "6.4": [],
+    "6.11": []
 }
