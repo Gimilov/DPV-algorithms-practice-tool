a81707c93110626d28d3c1f23ee0c759ef1034a9
Milosz Bator <miloszbator00@gmail.com>
2025-08-26 14:53:52 +0000

    corrected my solution for DPV 6.11

--- student_solutions/dpv_6_11.py
+++ student_solutions/dpv_6_11.py
@@ -23,9 +23,6 @@ def solution(args: tuple) -> int:
     # dp 
     for i in range(1, n+1):
         for j in range(1, m+1):
-            if x[i-1] == y[j-1]:
-                S[i][j] = 1 + S[i-1][j-1]
-            else:
-                S[i][j] = max(S[i-1][j], S[i][j-1])
+            S[i][j] = max(S[i-1][j], S[i][j-1], S[i-1][j-1] + (x[i-1] == y[j-1]))
 
     return S[-1][-1]
